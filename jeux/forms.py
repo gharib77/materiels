f01d1bc979964af86019a0ef3d3c83a450f9d761
jouali <gharibjouali.gmail.com>
2021-04-30 16:53:33 +0200

    adddateentr

--- jeux/forms.py
+++ jeux/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from jeux.models import Personne
 
 class FormPers(forms.ModelForm):
+    date_entr = forms.DateField(
+        input_formats=['%d/%m/%Y'],
+        widget=forms.DateInput(format='%d/%m/%Y')
+    )
     class Meta:
         model=Personne
         fields = '__all__'
--- jeux/models.py
+++ jeux/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Personne(models.Model):
     nom=models.CharField(max_length=50)
     prenom=models.CharField(max_length=50)
+    date_entr=models.DateField(auto_now=False,auto_now_add=False,null=True)
     class Meta:
         db_table='personnes'
     def __str__(self):
