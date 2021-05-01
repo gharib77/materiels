9ba036eb9125ec1919c532352c1703aff474ef9b
jouali <gharibjouali.gmail.com>
2021-05-01 13:13:08 +0200

    ajout table genre

--- jeux/models.py
+++ jeux/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    libelle=models.CharField(max_length=50)
+    class Meta:
+        db_table='genres'
+    def __str__(self):
+        return self.libelle
+
 class Personne(models.Model):
     nom=models.CharField(max_length=50)
     prenom=models.CharField(max_length=50)
     date_entr=models.DateField(auto_now=False,auto_now_add=False,null=True)
+    genre=models.ForeignKey(Genre,on_delete=models.CASCADE,null=True)
     class Meta:
         db_table='personnes'
     def __str__(self):
